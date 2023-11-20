8386d007cc891a1c73d749e245f69060771ae384
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-20 18:03:06 +0200

    squash!

--- packages/vscode-js-profile-flame/Source/client/common/constants.ts
+++ packages/vscode-js-profile-flame/Source/client/common/constants.ts
@@ -3,12 +3,12 @@
  *--------------------------------------------------------*/
 
 export const enum Constants {
-  BoxHeight = 20,
-  TextColor = '#fff',
-  BoxColor = '#000',
-  TimelineHeight = 22,
-  TimelineLabelSpacing = 100,
-  MinWindow = 0.005,
-  ExtraYBuffer = 300,
-  DefaultStackLimit = 7,
+	BoxHeight = 20,
+	TextColor = "#fff",
+	BoxColor = "#000",
+	TimelineHeight = 22,
+	TimelineLabelSpacing = 100,
+	MinWindow = 0.005,
+	ExtraYBuffer = 300,
+	DefaultStackLimit = 7,
 }
